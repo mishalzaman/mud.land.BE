5c90c954d7e97f4fe8c90d7b2a8b11a47ff5a31b
mishalzaman <mishalzaman@gmail.com>
2025-03-21 21:17:26 -0400

    add temp cors

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem "thruster", require: false
 
 gem 'active_model_serializers'
 
+gem 'rack-cors', require: 'rack/cors'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,15 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:5173'  # allow Vite frontend
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+end
